f762e41f08446ced09d79b7aaaa212c29463f2c0
Panda Naut <pandanautinspace@gmail.com>
2017-03-15 20:27:58 +0000

    trying to fix

--- js/main.js
+++ js/main.js
@@ -2,4 +2,4 @@ var d = document;
 var can = d.getElementById("gameCanvas");
 var ctx = can.getContext('2d');
 var nov = new Image('imgs/nov.jpg');
-ctx.drawImage(nov, 0, 0)
+ctx.drawImage(nov, 0, 0);
