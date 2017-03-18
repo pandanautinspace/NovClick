3b236aa26b33b2803de90a665f3aef024fa1198f
= <pandanautinspace@gmail.com>
2017-03-18 15:32:16 -0700

    Tried to make it load properly

--- js/main.js
+++ js/main.js
@@ -2,4 +2,10 @@ var d = document;
 var can = d.getElementById("gameCanvas");
 var ctx = can.getContext('2d');
 var nov = new Image('imgs/nov.jpg');
-ctx.drawImage(nov, 0, 0);
+nov.onLoad = function(){
+        nov.ready = true;
+}
+function render() {
+        ctx.drawImage(nov, 0, 0);
+        window.requestAnimationFrame(render);
+}
