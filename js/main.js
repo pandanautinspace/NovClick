f8dfdf6a366f7cf0e68654c4a2640cb09f15982a
2sodiumsandwich <325999@bsd48.org>
2017-03-20 11:01:15 -0700

    Added my crap
    did i do it?

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
-var d = document;
-var can = d.getElementById("gameCanvas");
-var ctx = can.getContext('2d');
-var nov = new Image('imgs/nov.jpg');
-ctx.drawImage(nov, 0, 0);
+var canvas = document.getElementById("gameCanvas");
+var context = canvas.getContext('2d');
+var nov = document.getElementById('nov');
+
+
+var width = 700, height = 400;
+var novvy = {
+	x: 0,
+	y: 0,
+}
+
+function update(){
+	novvy.x += 1
+
+}
+function render(){
+	context.clearRect(0, 0, width, height);
+	context.drawImage(nov, novvy.x, 9);
+}
+
+function game(){
+	update();
+	render();
+}
+
+setInterval(function(){
+	game();
+}, 1000/30);
